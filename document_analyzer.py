0ed4bad682fdbc5205926545f2f8727b45f0e572
HibikeQuantum <jadetypehoon@gmail.com>
2022-08-19 17:11:46 +0900

    Refactoring main function flow this work is created based on #18

--- document_analyzer.py
+++ document_analyzer.py
@@ -1,14 +1,12 @@
 # package for analyze
 from konlpy.tag import Kkma
-# There are other options, for example Okt, Komoran, Hannanum
+# inlcude Kkma, There are other options, ex. Okt, Komoran, Hannanum
 from collections import  Counter
 
 # package for file read /write feat.
-import json
-import re
-import csv
-import os, datetime
+import json, re, csv, os, datetime, sys
 from git import Repo
+
 
 """
 NOT USED NOW
@@ -19,15 +17,28 @@ NOT USED NOW
 # from nltk import regexp_tokenize
 # from nltk.corpus import gutenberg
 
-""" data structure preview """
+""" 
+# recipe data structure preview 
+
 rowRecipe = {
   "FileName":"",
-  "Date":"",
+  "Date":"", 
   "TotalCharacters": 0,
   "TotalKeyward": [],
   "ChangedCharacters": 0,
   "ChangedKeyward":[],
 }
+"""
+
+""" CONST DEFINE AND INITIALIZE """
+PWD = os.path.dirname(os.path.abspath(__file__))
+CONFIG_JSON_PATH = os.path.join(PWD, "config/config.json")
+DATA_JSON_PATH = os.path.join(PWD, "config/data.json")
+STATS_CSV_PATH = os.path.join(PWD, "Working/Statiscal_data/publishData.csv")
+KEYWORD_NUMBERS = 10
+
+""" Global Variables"""
+
 
 """
 NOT USED NOW
@@ -69,115 +80,135 @@ def getMostUsedWords(text):
   keywords = keywords[0:-1]
   return keywords
 
+
 def getFileTextLen(path):
   with open(path,'r',encoding='UTF8') as file:
     text = file.read()
     splitdata = text.split()
     return len(splitdata)
 
+
 def analyzeWholeText(path):
   with open(path,'r',encoding='UTF8') as file:
     text = file.read()
     return getMostUsedWords(text)
 
-# CONFIG VALUE
-pwd = os.path.dirname(os.path.abspath(__file__))
-configJsonPath = os.path.join(pwd, "config/config.json")
-dataJsonPath = os.path.join(pwd, "config/data.json")
-statisticalPath = os.path.join(pwd, "Working/Statiscal_data/publishData.csv")
-gitPath = None
 
-# CONSTANT AND INITIALIZE
-KEYWORD_NUMBERS = 10
-csvQueue = []
+def initializeData():
+  if not os.path.isfile(STATS_CSV_PATH):
+    print("[WANRNING] App did not found. csv File. App will make empty file.")
+    with open(STATS_CSV_PATH, "w", encoding = 'utf-8', newline='') as dataCsv:
+      print("[INFO]", STATS_CSV_PATH, " is maiden!")
 
-if not os.path.isfile(statisticalPath):
-  print("App did not found. csv File. App will make initial file.")
-  with open(statisticalPath, "w", encoding = 'utf-8', newline='') as dataCsv:
-    print(statisticalPath, "is maiden!")
+  with open(CONFIG_JSON_PATH, "r", encoding='utf-8') as configJsonFile:
+    global GIT_PATH
+    configJson = json.load(configJsonFile)
+    GIT_PATH = configJson['gitPath']
 
-with open(configJsonPath, "r", encoding='utf-8') as configJsonFile:
-  configJson = json.load(configJsonFile)
-  gitPath = configJson['gitPath']
-
-with open(dataJsonPath, "r", encoding='utf-8') as dataJsonFile:
-  writtenFileNameList = json.load(dataJsonFile)['written_file_names']
-
-# Main Logic is Started here.
-# TODO: Make Main function
-repository = Repo(gitPath, search_parent_directories=True)
-target_diff_index_array = []
-Hcommit = repository.head.commit
-DiffIndex = Hcommit.diff('HEAD~1', create_patch=True, unified=0)
-
-for diff_index_M in DiffIndex.iter_change_type('M'):
-  target_diff_index_array.append(diff_index_M)
-for diff_index_A in DiffIndex.iter_change_type('A'):
-  print("_A:", diff_index_A)
-  target_diff_index_array.append(diff_index_A)
+  #### Not used NOW
+  # with open(DATA_JSON_PATH, "r", encoding='utf-8') as dataJsonFile:
+  #   writtenFileNameList = json.load(dataJsonFile)['written_file_names']
 
 
-if len(target_diff_index_array) == 0:
-  print("Nothing todo")
+def getChangedDiffIndexArray():
+  resultArr = []
 
-for diff_index in target_diff_index_array:
-  file_path = os.path.join(gitPath,diff_index.b_path)
-  wholeSendtences = diff_index.diff.decode('utf-8')
+  repository = Repo(GIT_PATH, search_parent_directories=True)
+  headCommit = repository.head.commit
+  Diffs = headCommit.diff('HEAD~1', create_patch=True, unified=0)
 
-  # initialize collecting data
-  sentence = ""
-  rowRecipe = {}
-  newCharacterNumbers = 0
-  previouChar = ""
-  # iterate git-diff-lines
-  for (index, line) in enumerate(wholeSendtences.split("\n")):
-    # ignore option line.
-    if line[-2:] == "@@":
-      headInfoArr = line.split(" ")
-      newCharacterNumbers = headInfoArr[2][1:]
-      index = newCharacterNumbers.find(",")
-      if index != -1:
-        newCharacterNumbers = newCharacterNumbers[0:index]
-      continue
-    
-    matchedLine = re.match(r'^\+(.*)', line)
-    striptedString = ""
-    if previouChar == "-" and line[0:1] == "-":
-      matchedLine = re.match(r'^\-(.*)', line)
+  for diff_index_M in Diffs.iter_change_type('M'):
+    print("TYPE _M:", diff_index_M)
+    resultArr.append(diff_index_M)
+  for diff_index_A in Diffs.iter_change_type('A'):
+    print("TYPE _A:", diff_index_A)
+    resultArr.append(diff_index_A)
+  
+  if len(resultArr) == 0:
+    print("[INFO] In git diff index, There is no change that worthy of reference. So nothing todo.")
+    sys.exit(0)
+  else:
+    return resultArr
+
+
+def anlayzeDiffIndex(diff_index_array):
+  recipeArray = []
+
+  for diff_index in diff_index_array:
+    filePath = os.path.join(GIT_PATH, diff_index.b_path)
+    wholeSendtences = diff_index.diff.decode('utf-8')
+
+    # initialize collecting data
+    sentence = ""
+    rowRecipe = {}
+    newCharacterNumbers = 0
+    previouChar = ""
+
+    # iterate git-diff-lines
+    for (index, line) in enumerate(wholeSendtences.split("\n")):
+      # ignore option line.
+      if line[-2:] == "@@":
+        headInfoArr = line.split(" ")
+        newCharacterNumbers = headInfoArr[2][1:]
+        index = newCharacterNumbers.find(",")
+        if index != -1:
+          newCharacterNumbers = newCharacterNumbers[0:index]
+        continue
       
-    if matchedLine is not None:
-      matchedLine = matchedLine.group(1)
-      striptedString = line.lstrip()
-      sentence += matchedLine
-    previouChar = line[0:1]
+      # if line is not meta data, find vaild line.
+      # In code block. git print '-' each line. These formatting policy makes a mess of logic. So 'previouChar' is used to avoid the malfunctioning.
+      matchedLine = re.match(r'^\+(.*)', line)
+      if previouChar == "-" and line[0:1] == "-":
+        matchedLine = re.match(r'^\-(.*)', line)
 
-  rowRecipe['FileName'] = file_path
-  rowRecipe['Date'] =  datetime.datetime.now()
-  rowRecipe['TotalCharacters'] = getFileTextLen(file_path)
-  rowRecipe['TotalKeyward'] = analyzeWholeText(file_path)
-  rowRecipe['ChangedCharacters'] = newCharacterNumbers
-  rowRecipe['ChangedKeyward'] = getMostUsedWords(sentence)
+      if matchedLine is not None:
+        matchedLine = matchedLine.group(1)
+        sentence += matchedLine
+      previouChar = line[0:1]
 
-  """
-  TODO: english word is ignored. in these steps. It could be enhanced.
-  # rowRecipe['TotalKeyward'] = analyzeEngDoc(file_path)
-  """
-  csvQueue.append(rowRecipe)
+    #end one diff index.
+    rowRecipe['FileName'] = filePath
+    rowRecipe['Date'] =  datetime.datetime.now()
+    rowRecipe['TotalCharacters'] = getFileTextLen(filePath)
+    rowRecipe['TotalKeyward'] = analyzeWholeText(filePath)
+    rowRecipe['ChangedCharacters'] = newCharacterNumbers
+    rowRecipe['ChangedKeyward'] = getMostUsedWords(sentence)
+    
+    recipeArray.append(rowRecipe)
 
-# Write file
-with open(statisticalPath, "a", encoding = 'utf-8') as dataCsv:
-  wr = csv.writer(dataCsv)
-  for dict in csvQueue:
-    wr.writerow([
-    dict['FileName'],
-    dict['Date'],
-    dict['TotalCharacters'],
-    dict['TotalKeyward'],
-    dict['ChangedCharacters'],
-    dict['ChangedKeyward']
-    ])
+    """
+    TODO: english word is ignored. in these steps. It could be enhanced.
+    # rowRecipe['TotalKeyward'] = analyzeEngDoc(filePath)
+    """
 
-print("_______________________________________________________")
-print("Write is over! ", len(csvQueue), " rows are inserted at CSV")
+  #end every diff indexes.
+  print("[DONE] analyze process is done")
+  return recipeArray
 
 
+def appendDataToCSV(recipeArr):
+  with open(STATS_CSV_PATH, "a", encoding = 'utf-8') as dataFile:
+    writerObj = csv.writer(dataFile)
+    for dict in recipeArr:
+      writerObj.writerow([
+      dict['FileName'],
+      dict['Date'],
+      dict['TotalCharacters'],
+      dict['TotalKeyward'],
+      dict['ChangedCharacters'],
+      dict['ChangedKeyward']
+      ])
+      
+  print("_______________________________________________________")
+  print("[INFO] Write is over! ", len(recipeArr), " rows are inserted at CSV")
+
+
+def main():
+  initializeData()
+  diff_index_array = getChangedDiffIndexArray()
+  csvQueue = anlayzeDiffIndex(diff_index_array)
+  appendDataToCSV(csvQueue)
+
+
+if __name__ == '__main__':
+  main()
